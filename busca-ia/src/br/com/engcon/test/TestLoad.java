3958956c88024ca01d084bd54a48ceba2d46f1ec
Rubens Diniz <rubensadiniz@edu.unifor.br>
2018-08-19 20:56:58 -0300

    Feita questão 6 (sem AVL)

--- busca-ia/src/br/com/engcon/test/TestLoad.java
+++ busca-ia/src/br/com/engcon/test/TestLoad.java
@@ -1,6 +1,9 @@
 package br.com.engcon.test;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import br.com.engcon.graph.Graph;
 import br.com.engcon.graph.Node;
@@ -9,15 +12,36 @@ import br.com.engcon.util.Import;
 public class TestLoad {
 	
 	public static void main(String[] args) throws Exception {
-		
+
+		//"bus-network" ou "caxeiro"
 		Graph graph = Import.loadGraph("bus-network");
 		
 		List<Node> nodes = graph.getNodeList();
 
-        System.out.println("Quntidade de nós é " + graph.size());
-        System.out.println("A decidade é "+ ((float)graph.getEdges().size())/graph.size());
-		System.out.println("O grau médio é "+ graph.averageDegree());
-		
+		//Q1-Q4
+        System.out.println("1. A quantidade de nós (ordem) é " + graph.size());
+		System.out.println("2. A quantidade de arestas (tamanho) é " + (float)graph.getEdges().size());
+        System.out.println("3. A densidade é "+ ((float)graph.getEdges().size())/graph.size());
+		System.out.println("4. O grau médio é "+ graph.averageDegree());
+
+		//Q6
+		List<Integer> degrees = new ArrayList<>();
+		int biggestValue = 0;
+
+		for (Node n : graph.getNodeList()){
+			degrees.add(n.getDegree());
+			if (n.getDegree() > biggestValue) biggestValue = n.getDegree();
+		}
+
+		System.out.print("6. Distribuição de graus (grau/quantidade): ");
+		for (int i = 0; i <= biggestValue; i++){
+			if (Collections.frequency(degrees, i) > 0) System.out.print("(" + i + "/" + Collections.frequency(degrees, i) + ")");
+		}
+		System.out.println();
+
+
+		//5. Quantidade de cliques >= 4 vértices
+		//7. Distribuição dos pesos nas arestas
 	}
 
 }
